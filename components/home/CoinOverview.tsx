a6a0fa401245fe7a6a38a6f02d5a35bee6c88b52
ParthDevInnovator <makwanaunofficialparth@gmail.com>
2026-01-20 13:09:28 +0530

    main

--- components/home/CoinOverview.tsx
+++ components/home/CoinOverview.tsx
@@ -3,23 +3,30 @@ import Image from 'next/image';
 import { formatCurrency } from '@/lib/utils';
 import { CoinOverviewFallback } from './fallback';
 import { fetcher } from '@/lib/coingetko.actions';
+import { interval } from 'date-fns';
+import Candlestickchart from '../CandlestickChart';
 
 
 const CoinOverview = async () => {
   let coin;
+  let coinOHLCdata;
 
   try {
-    coin = await fetcher<CoinDetailsData>('/coins/bitcoin', {
-      dex_pair_format: 'symbol',
-    });
-  } catch (error) {
-    console.error('Error fetching coin overview:', error);
-    return <CoinOverviewFallback />;
-  }
-
-  return (
+    [coin, coinOHLCdata] = await Promise.all([
+      fetcher<CoinDetailsData>('/coins/bitcoin', {
+        dex_pair_format: 'symbol',
+      }),
+      fetcher<OHLCData>('/coins/bitcoin/ohlc', {
+        vs_currency: 'usd',
+        days: 1,
+        interval: 'hourly',
+        precision: 'full',
+      }),
+    ]);
+    return (
     <div id="coin-overview">
-      <div className="header pt-2">
+      <Candlestickchart data={coinOHLCdata} coinId="bitcoin">
+          <div className="header pt-2">
         <Image src={coin.image.large} alt={coin.name} width={56} height={56} />
         <div className="info">
           <p>
@@ -28,8 +35,17 @@ const CoinOverview = async () => {
           <h1>{formatCurrency(coin.market_data.current_price.usd)}</h1>
         </div>
       </div>
+      </Candlestickchart>
+      
     </div>
   );
+
+  } catch (error) {
+    console.error('Error fetching coin overview:', error);
+    return <CoinOverviewFallback />;
+  }
+
 };
+
 
 export default CoinOverview;
